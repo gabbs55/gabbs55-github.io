5ab6d58236b9f21552a4eda8816f05a61756ad81
gabrielle <gabby.ulett@gmail.com>
2018-06-08 15:29:00 +0000

    graffiti project

---
+++ buttom.js
@@ -0,0 +1,7 @@
+function loudhouse(){
+var thatwaseasy=new Audio("loudhouse.mp3")
+thatwaseasy.play();
+}
+
+
+"("#easy").on(on("click",loudhouse.mp3);
---
+++ main.js/main.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>That Was Easy</title>
+            <link href="style.css" rel="stylesheet">
+        </head>
+        <body>
+            <button> id="easy">Easy</button>
+            <script scr="https://code.jquery-2.2.3.min.js"></script>
+            <script scr="main.js"></script>
+        </body>
+    </html>
